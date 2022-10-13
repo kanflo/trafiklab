8063928e1d4cffee574ba7de9d099c8303e76c70
Johan Kanflo <johan.kanflo@bitfuse.net>
2022-10-13 08:30:02 +0200

    Added comments to example

--- test/tripmon.py
+++ test/tripmon.py
@@ -23,8 +23,11 @@ logger = logging.getLogger(__name__)
 coloredlogs.install(level="DEBUG" if "-d" in sys.argv else "INFO", fmt='%(asctime)s,%(msecs)03d %(name)s %(levelname)-8s %(filename)s:%(lineno)d %(message)s')
 
 t = trafiklab.tripmonitor()
+# No use in presenting buses we cannot catch unless we run for our lives
 time_to_walk_to_the_bus_stop_in_minutes = 15
 t.init(time_to_walk_to_the_bus_stop_in_minutes, api_key)
+# Partial names are supported. The full names below are
+# "Dalby busstation (Lund kn)" and "Lund Centralstation"
 if not t.add_route("Dalby Buss", "Lund Central"):
     logging.error("Failed to add route")
     sys.exit(1)
